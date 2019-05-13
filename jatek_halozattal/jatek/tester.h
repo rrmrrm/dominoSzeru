9a3f8708f41411a5710ff22b5fb87219ff49fbea
Radnai Miklós <radnaimiki@gmail.com>
2019-05-14 00:59:27 +0200

    -

---
+++ jatek_halozattal/jatek/tester.h
@@ -0,0 +1,22 @@
+#ifndef TESTER_H
+#define TESTER_H
+
+#include <QObject>
+
+#include <QWidget>
+
+class Tester : public QObject{
+    Q_OBJECT
+public:
+
+
+private slots:
+    void initTestCase() {
+
+    }
+    void cleanupTestCase() {
+
+    }
+};
+
+#endif // TESTER_H
